d09d95846334d3cebc64960642051385d21b1028
Brian <knollbs@gmail.com>
2015-09-11 05:05:42 -0700

    Alternate checksum method, works in-place and imports Hales' code instead of pasting checksum generator into the node source

--- ugv_nav/src/motordata_send.cpp
+++ ugv_nav/src/motordata_send.cpp
@@ -5,12 +5,16 @@
 #include <ros/ros.h>
 #include <ros/rate.h>
 #include <cmath>
+#include "shared/shared.hpp"
 
 #define MOTORDRIVER_RANGE_MAX 255
+#define MOTORDRIVER_COMBUFFER_MSGLENGTH 6
+#define MOTORDRIVER_COMBUFFER_CHKSUMDISTFROMEND 3
+#define MOTORDRIVER_COMBUFFER_CHKSUMLENGTH 2
 #define MOTORDRIVER_COMBUFFER_LENGTH 11
 
 using namespace std;
-int createChecksum (char * message, int len);
+
 
 /* This program will read values from /heading and /magnitude,
  * convert them to motor driver input and send them to
@@ -36,15 +40,15 @@ int main(int argc, char** argv){
    // Loop until this node is stopped (using ctrl-c)
    while(ros::ok()){
       float theta;
-      float r;
+      float r; 
       double theta_d;
-      double r_d;
+      double r_d; 
 
       // Get the data
       ros::param::getCached("/heading", theta_d);
       ros::param::getCached("/magnitude", r_d);
       ROS_INFO_STREAM("theta " << theta_d << " mag " << r_d);
-
+      
       // Convert to motor coordinates
       //  * Half the range is dedicated to forward motion
       //  * Half the range is dedicated to turning
@@ -57,12 +61,10 @@ int main(int argc, char** argv){
       leftMotorVal = abs(leftMotorVal);
       rightMotorVal = abs(rightMotorVal);
 
-      char createsum[MOTORDRIVER_COMBUFFER_LENGTH];
-      snprintf(buffer, MOTORDRIVER_COMBUFFER_LENGTH, "[%c%2x%c%2x:]", leftMotorSign, leftMotorVal, rightMotorSign, rightMotorVal);
-      int checksum = createChecksum(createsum,MOTORDRIVER_COMBUFFER_LENGTH);
-
       char buffer[MOTORDRIVER_COMBUFFER_LENGTH];
-      snprintf(buffer, MOTORDRIVER_COMBUFFER_LENGTH, "[%c%2x%c%2x:%2x]", leftMotorSign, leftMotorVal, rightMotorSign, rightMotorVal, checksum);
+      snprintf(buffer, MOTORDRIVER_COMBUFFER_LENGTH, "[%c%2x%c%2x:00]", leftMotorSign, leftMotorVal, rightMotorSign, rightMotorVal);
+      char checksum = calculateChecksum(&buffer[1], MOTORDRIVER_COMBUFFER_MSGLENGTH);
+      snprintf(&buffer[(MOTORDRIVER_COMBUFFER_LENGTH - MOTORDRIVER_COMBUFFER_CHKSUMDISTFROMEND)], MOTORDRIVER_COMBUFFER_CHKSUMLENGTH, "%2x", checksum);
 
       // Write these bytes to the Com Port
       comPort.write(buffer, MOTORDRIVER_COMBUFFER_LENGTH);
@@ -79,45 +81,3 @@ int main(int argc, char** argv){
    return 0;
 
 }
-
-int createChecksum (char * message, int len){
-   unsigned char primes[] = {  2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
-                           31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
-                             73,  79,  83,  89,  97, 101, 103, 107, 109, 113,
-                            127, 131, 137, 139, 149, 151, 157, 163, 167, 173 };
-  int checksum = 0;
-  for (unsigned char i=0; i < len; i++ )
-  {
-    if ( message[i] == ':' ) break; // Seperates message body from checksum
-    checksum += message[i] * primes[i];
-  }
-  return checksum % 256;
-}
-
-/* // This does the background checks in the motor driver for the checksum
-bool numberAsign = ( command->text[0] == '-' );
-unsigned char numberA  = hexToNum( command->text[1] ) * 16 + hexToNum( command->text[2] );
-bool numberBsign = ( command->text[3] == '-' );
-unsigned char numberB  = hexToNum( command->text[4] ) * 16 + hexToNum( command->text[5] );
-unsigned char claimedChecksum = hexToNum( command->text[7] ) * 16 + hexToNum( command->text[8] );
-*/
-
-
-
-
-/* This is the check sum calculator for the motor driver
-unsigned char calculateChecksum( char * message, int len)
-{
-  unsigned char primes[] = {  2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
-                             31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
-                             73,  79,  83,  89,  97, 101, 103, 107, 109, 113,
-                            127, 131, 137, 139, 149, 151, 157, 163, 167, 173 };
-  int checksum = 0;
-  for (unsigned char i=0; i < len; i++ )
-  {
-    if ( message[i] == ':' ) break; // Seperates message body from checksum
-    checksum += message[i] * primes[i];
-  }
-  return checksum % 256;
-}
-*/
