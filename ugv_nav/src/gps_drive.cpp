373f6d91abc4605d1d8874f6c78e1801134f1cfe
Aneita <aneita.yang@gmail.com>
2015-10-09 13:17:42 +1100

    print statements

--- ugv_nav/src/gps_drive.cpp
+++ ugv_nav/src/gps_drive.cpp
@@ -75,9 +75,12 @@ void GPS_Drive::gps_callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
    source_longitude = msg->longitude;
 
    printf("Current GPS fix,  lat: %lf, long: %lf\n", source_latitude, source_longitude);
+   printf("We want to go to destination,  lat: %lf, long: %lf\n", destination_latitude, destination_longitude);
 
    double difference_lat = destination_latitude - source_latitude;
    double difference_long =  destination_longitude - source_longitude;
+
+   printf("Difference in,  lat: %lf, long: %lf\n", difference_lat, difference_long);
 
    distance_to_go = sqrt(difference_lat*difference_lat+difference_long*difference_long)*111000.0;
    // note: clockwise is positive direction
